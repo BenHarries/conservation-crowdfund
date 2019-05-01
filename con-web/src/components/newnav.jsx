8a8e45c209f44ad014df3db80c192d6426ab1344
Ben Harries <benkelvinharries@gmail.com>
2019-05-01 22:09:51 +0100

    took out false api line

--- con-web/src/components/newnav.jsx
+++ con-web/src/components/newnav.jsx
@@ -10,10 +10,6 @@ import Turtle from "./project";
 import SearchBar from "./searchbar";
 import "./newnav.css";
 
-const API =
-  "http://apiv3.iucnredlist.org/api/v3/species/loxodonta%20africana?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee";
-// const API = "https://randomuser.me/api/?results=500";
-
 export default class NavBar extends Component {
   state = { users: [], isLoading: false, error: null, pictures: [] };
 
@@ -29,24 +25,6 @@ export default class NavBar extends Component {
       })
       .then(users => this.setState({ users, isLoading: false }))
       .catch(error => this.setState({ error, isLoading: false }));
-    fetch("http://localhost:3001" + API)
-      .then(result => {
-        if (result.ok) {
-          return result.json();
-        } else {
-          throw new Error("Something went wrong...");
-        }
-      })
-      .then(data => {
-        let pictures = data.result.map(pic => {
-          return (
-            <div key={pic.results}>
-              <p>{pic.category}</p>
-            </div>
-          );
-        });
-        this.setState({ pictures: pictures });
-      });
   }
   handleContextRef = contextRef => this.setState({ contextRef });
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
